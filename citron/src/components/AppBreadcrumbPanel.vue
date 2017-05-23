6635e79ed6ef0e1aad84d3abec4519f9be9201c5
sharonkrishh-lad <krishna>
2017-05-23 12:45:52 +0545

    breadcrumb and action panel made

--- citron/src/components/AppBreadcrumbPanel.vue
+++ citron/src/components/AppBreadcrumbPanel.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="row">
     <div class="col-xs-12">
-      <div class="panel__title__box">
+      <div class="panel__title__row">
         <h1>Dashboard</h1>
         <ol class="breadcrumb">
           <li><a href="#">Home</a></li>
           <li><a href="#">Hr module</a></li>
           <li class="active">Dashboard</li>
         </ol>
+      </div>
+      <div class="app__actions__panel">
+        <a href="" class="button button--green">Add new Hr</a>
       </div>
     </div>
   </div>
@@ -25,11 +28,29 @@ export default {
 </script>
 
 <style scoped>
-.panel__title__box{
-
+.panel__title__row{
+  margin-bottom: 20px;
 }
-.panel__title__box ul{
+.panel__title__row h1{
+  margin: 0 0 5px 0;
+  font-size: 20px;
+  font-weight: 500;
+}
+.panel__title__row .breadcrumb{
   margin: 0;
   padding: 0;
+}
+.panel__title__row .breadcrumb li,.panel__title__row .breadcrumb li a{
+  font-size: 12px;
+  font-weight: normal;
+  color: #8a8a8a;
+} 
+.panel__title__row .breadcrumb li.active:before{
+  color: #2ecc71;
+}
+.app__actions__panel{
+  position: absolute;
+  right: 15px;
+  top: 10px;
 }
 </style>
