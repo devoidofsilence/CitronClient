e3eff111679d3419733f18ab125d9c2713184dc2
sharonkrishh-lad <krishna>
2017-05-30 12:35:24 +0545

    css updates

--- citron/src/components/EmployeeListCard.vue
+++ citron/src/components/EmployeeListCard.vue
@@ -92,24 +92,4 @@ height: 100%;
   font-size: 26px;
   font-weight: 300;
 }
-
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
-  opacity: 0
-}
-/* Enter and leave animations can use different */
-/* durations and timing functions.              */
-.slide-fade-enter-active {
-  transition: all .3s ease;
-}
-.slide-fade-leave-active {
-  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-}
-.slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active for <2.1.8 */ {
-  transform: translateX(400px);
-  opacity: 0;
-}
 </style>
--- citron/src/components/EmployeesList.vue
+++ citron/src/components/EmployeesList.vue
@@ -177,23 +177,4 @@ height: 100%;
   color: #8c8c8c;
   font-size: 16px;
 }
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
-  opacity: 0
-}
-/* Enter and leave animations can use different */
-/* durations and timing functions.              */
-.slide-fade-enter-active {
-  transition: all .3s ease;
-}
-.slide-fade-leave-active {
-  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-}
-.slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active for <2.1.8 */ {
-  transform: translateX(400px);
-  opacity: 0;
-}
 </style>
