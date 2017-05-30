7a1213270ebc41c939ee6a3553964576b0d7652c
sauravrb <sauravrajbahak@gmail.com>
2017-05-30 10:52:30 +0545

    project create

--- citron/src/components/ProjectLists.vue
+++ citron/src/components/ProjectLists.vue
@@ -1,10 +1,13 @@
 
 <template>
+ 
   <div class="projectListView__credentials">
-    <RightSlideCanvas2></RightSlideCanvas2>
+       <transition name="slide-fade">
+                <RightSlideCanvas v-if="showHideRightPanel" @close="closeNav" :canvas="canvasType"></RightSlideCanvas>
+            </transition>
       <div class="app__actions__panel app__actions__panelStatus">
         <button class="button button--border--green statusSearchBtn" v-on:click="show = !show">Search      projects</button>
-          <span class="button button--green" id="sidebar-main-trigger" v-on:click="openNav()">Add new project</span>
+          <span class="button button--green" id="sidebar-main-trigger" v-on:click="openNav">Add new project</span>
       </div>
 
    <section class="employees__list__row">
@@ -359,20 +362,21 @@
 
 
 <script>
-import RightSlideCanvas2 from './RightSlideCanvas2'
+import RightSideCanvas from './RightSlideCanvas.vue'
 export default {
   name: 'ProjectLists',
   components: {
-    RightSlideCanvas2
+    RightSideCanvas
   },
   data () {
     return {
-      msg: 'Citron'
+      msg: 'Citron',
+      showHideRightPanel: false
     }
   },
 methods: {
-      openNav:function () {
-      document.getElementById('rightSideCanvas2').style.width = '45%'
+       openNav: function () {
+      this.showHideRightPanel = true
       document.body.className = 'bodyOpenCanvas'
     }
   }
