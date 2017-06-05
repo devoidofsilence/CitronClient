1cb1fed2ca6445c1060bad5327ae1641fd3c1ecb
sharonkrishh-lad <krishna>
2017-06-05 11:13:03 +0545

    loading css and html has updated

--- citron/src/App.vue
+++ citron/src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <div class="loadingMask">
+      <div class="loader">
+        <div class="loader__figure"></div>
+        <p class="loader__label">Loading</p>
+      </div>
+    </div>
     <SiteHeader></SiteHeader>
     <AsidePanelMenu></AsidePanelMenu>
     <ContentArea></ContentArea>
