c514d5d6a0d582b5721ec89c69297c56890ee66f
sharonkrishh-lad <krishna>
2017-06-06 09:24:45 +0545

    changes commit

--- citron/src/App.vue
+++ citron/src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
     <div class="loadingMask" style="display:none;">
-    <div v-if="showHideLoader" class="loadingMask">
-      <div class="loader">
-        <div class="loader__figure"></div>
-        <p class="loader__label">Loading</p>
+      <div v-if="showHideLoader" class="loadingMask">
+        <div class="loader">
+          <div class="loader__figure"></div>
+          <p class="loader__label">Loading</p>
+        </div>
       </div>
     </div>
     <SiteHeader></SiteHeader>
